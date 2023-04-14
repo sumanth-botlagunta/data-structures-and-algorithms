576eab434f586d0b1b041b08f825087cd857c0af
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2023-04-14 22:50:43 +0530

    indentation

--- longest common subsequence/lcs.cpp
+++ longest common subsequence/lcs.cpp
@@ -4,23 +4,32 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int longestCommonSubsequence(string text1, string text2) {
-        int n = text1.length();
-        int m = text2.length();
-        int dp[n+1][m+1];
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= m; j++) {
-                if (i == 0 || j == 0) {
-                    dp[i][j] = 0;
-                } else if (text1[i-1] == text2[j-1]) {
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                } else {
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-                }
-            }
+  int longestCommonSubsequence(string text1, string text2)
+  {
+    int n = text1.length();
+    int m = text2.length();
+    int dp[n + 1][m + 1];
+    for (int i = 0; i <= n; i++)
+    {
+      for (int j = 0; j <= m; j++)
+      {
+        if (i == 0 || j == 0)
+        {
+          dp[i][j] = 0;
         }
-        return dp[n][m];
+        else if (text1[i - 1] == text2[j - 1])
+        {
+          dp[i][j] = 1 + dp[i - 1][j - 1];
+        }
+        else
+        {
+          dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+      }
     }
+    return dp[n][m];
+  }
 };
